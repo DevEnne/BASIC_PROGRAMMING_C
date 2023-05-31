8b441fd82444082a438691d678a761b50e524a7b
KimJei <dev@earthquake.co.kr>
2023-06-01 00:55:28 +0900

    Create 2-2.c

---
+++ 2강/2-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+    int sum;
+
+  
+    x = 100;
+    y = 200;
+
+  
+    sum = x+y;
+    printf("두 수의 합 = %d\n", sum);
+
+  
+    return 0;
+}
